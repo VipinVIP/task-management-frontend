03c6d525a190d2037cf36ab30a2a2692bb26ca92
Vipin K P <58673683+VipinVIP@users.noreply.github.com>
2024-04-09 17:39:18 +0530

    feat: add theme toggle to navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+  isDark: boolean = false;
 
+ngOnInit() {
+  const storedValue = localStorage.getItem('isDark');
+  this.isDark = storedValue ? (storedValue === "true") : false;
+  this.applyTheme();
+}
+
+toggleTheme() {
+  this.isDark = !this.isDark;
+  localStorage.setItem('isDark', this.isDark.toString());
+  this.applyTheme();
+}
+
+private applyTheme() {
+  const htmlElement = document.querySelector('html');
+  if (htmlElement) {
+    htmlElement.setAttribute('data-theme', this.isDark ? "dark" : "light");
+  }
+}
 }
