554bb986719e9c67e54964b4266f38c0ea6f6315
Vipin K P <58673683+VipinVIP@users.noreply.github.com>
2024-04-15 12:57:28 +0530

    feat: bind add-task service to UI

--- src/app/components/task-form/task-form.component.ts
+++ src/app/components/task-form/task-form.component.ts
@@ -1,32 +1,47 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-task-form',
   standalone: true,
-  imports: [CommonModule,ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
+  providers: [TaskService],
   templateUrl: './task-form.component.html',
 })
 export class TaskFormComponent {
-
-  fb=inject(FormBuilder)
-  taskForm: FormGroup=this.fb.group({
+  fb = inject(FormBuilder);
+  taskService = inject(TaskService);
+  taskForm: FormGroup = this.fb.group({
     title: ['', Validators.required],
     description: [''],
     priority: ['', Validators.required],
     dueDate: ['', Validators.required],
-    assignees: [[]] 
-  })
+  });
   get f() {
     return this.taskForm.controls;
   }
   onSubmit() {
     if (this.taskForm.valid) {
-      console.log(this.taskForm.value);
+      const task = this.taskForm.value;
+      this.taskService.addTask(task).subscribe({
+        next: (resp) => {
+          console.log(resp);
+          this.taskForm.reset();
+        },
+        error: (error) => {
+          console.error(error);
+        },
+      });
+      console.log(task);
     } else {
       this.taskForm.markAllAsTouched();
     }
   }
-  
 }
