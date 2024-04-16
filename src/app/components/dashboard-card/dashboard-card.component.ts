842b835cfb47b28e0996cfdd9d8b85f1043ec250
ThimnaRaphel <thimnaraphel12@gmail.com>
2024-04-16 19:22:45 +0530

    feat: added get hook in service

--- src/app/components/dashboard-card/dashboard-card.component.ts
+++ src/app/components/dashboard-card/dashboard-card.component.ts
@@ -1,13 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+
+
+export interface Task {
+  id: number;
+  title: string;
+  description?: string;
+  priority: 'Low' | 'Medium' | 'High';
+  dueDate: Date;
+  progress: number;
+  user_id: number;
+}
 
 @Component({
   selector: 'app-dashboard-card',
   standalone: true,
   imports: [CommonModule],
+  providers:[TaskService],
   templateUrl: './dashboard-card.component.html',
   styleUrl: './dashboard-card.component.css'
 })
+
+
 export class DashboardCardComponent {
 
 }
--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { LoginComponent } from '../../components/login/login.component';
-import { TaskFormComponent } from '../../components/task-form/task-form.component';
+import { DashboardCardComponent } from '../../components/dashboard-card/dashboard-card.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule,LoginComponent,TaskFormComponent],
+  imports: [CommonModule,LoginComponent,DashboardCardComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
