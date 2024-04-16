1d7781cc83848667a6ee033f2628cafe35a8ddac
ThimnaRaphel <thimnaraphel12@gmail.com>
2024-04-16 15:26:58 +0530

    feat(page): added dashboard page

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { LoginComponent } from '../../components/login/login.component';
-import { TaskFormComponent } from '../../components/task-form/task-form.component';
+import { DashboardPageComponent } from '../dashboard-page/dashboard-page.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule,LoginComponent,TaskFormComponent],
+  imports: [CommonModule,LoginComponent,DashboardPageComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
