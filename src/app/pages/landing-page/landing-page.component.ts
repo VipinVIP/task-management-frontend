15fc485a1be47a5a23a475f5398428f8c63358ce
Vipin K P <58673683+VipinVIP@users.noreply.github.com>
2024-04-16 17:53:07 +0530

    fix: move add-task feature into a modal

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -2,16 +2,34 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { LoginComponent } from '../../components/login/login.component';
 import { TaskFormComponent } from '../../components/task-form/task-form.component';
+import { FormSubmissionResponse } from '../../types';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule,LoginComponent,TaskFormComponent],
+  imports: [CommonModule, LoginComponent, TaskFormComponent],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.css'
+  styleUrl: './landing-page.component.css',
 })
 export class LandingPageComponent {
-isLoggedIn(){
-  return localStorage.getItem('access_token')!==null
-}
+  isLoggedIn() {
+    return localStorage.getItem('access_token') !== null;
+  }
+  actionSuccess = false;
+  actionFailure = false;
+  onStatusChange(status: FormSubmissionResponse) {
+    console.log('from child to parent', status);
+    if (status.status == 'success') {
+      this.actionSuccess = true;
+      setTimeout(() => {
+        this.actionSuccess = false;
+      }, 3000);
+    }
+    if (status.status == 'failure') {
+      this.actionFailure = true;
+      setTimeout(() => {
+        this.actionFailure = false;
+      }, 3000);
+    }
+  }
 }
