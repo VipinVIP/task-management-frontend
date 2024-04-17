ec1adfc9537f30d2de8fa4d1eb07031fdb271575
Vipin K P <58673683+VipinVIP@users.noreply.github.com>
2024-04-17 08:38:36 +0530

    feat: implement wildcard routes to show 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { TaskFormComponent } from './components/task-form/task-form.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-  {path:'',component:LandingPageComponent},
-  {path:'signup',component:SignupComponent},
-  {path:'login',component:LoginComponent},
-  {path:'add-task',component:TaskFormComponent}
+  { path: '', component: LandingPageComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'add-task', component: TaskFormComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
