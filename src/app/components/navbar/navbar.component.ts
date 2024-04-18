32b5f70f034944606338890b187a87a1e7db616b
Vipin K P <58673683+VipinVIP@users.noreply.github.com>
2024-04-18 12:13:00 +0530

    fix: remove searchbar, and other QoL improvements.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,32 +1,34 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
   isDark: boolean = false;
+  @Input() isLoggedIn: boolean = false;
 
-ngOnInit() {
-  const storedValue = localStorage.getItem('isDark');
-  this.isDark = storedValue ? (storedValue === "true") : false;
-  this.applyTheme();
-}
-
-toggleTheme() {
-  this.isDark = !this.isDark;
-  localStorage.setItem('isDark', this.isDark.toString());
-  this.applyTheme();
-}
-
-private applyTheme() {
-  const htmlElement = document.querySelector('html');
-  if (htmlElement) {
-    htmlElement.setAttribute('data-theme', this.isDark ? "dark" : "light");
+  ngOnInit() {
+    const storedValue = localStorage.getItem('isDark');
+    this.isDark = storedValue ? storedValue === 'true' : false;
+    this.applyTheme();
   }
-}
+
+  toggleTheme() {
+    this.isDark = !this.isDark;
+    localStorage.setItem('isDark', this.isDark.toString());
+    this.applyTheme();
+  }
+
+  private applyTheme() {
+    const htmlElement = document.querySelector('html');
+    if (htmlElement) {
+      htmlElement.setAttribute('data-theme', this.isDark ? 'dark' : 'light');
+    }
+  }
 }
