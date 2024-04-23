978be76e5157f678246abef3370eeaec085ce669
ThimnaRaphel <thimnaraphel12@gmail.com>
2024-04-23 09:36:41 +0530

    feat:added home page and changed the favicon icon

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { LoginComponent } from '../../components/login/login.component';
 import { DashboardPageComponent } from '../dashboard-page/dashboard-page.component';
+import { HomepageComponent } from '../homepage/homepage.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule, LoginComponent, DashboardPageComponent],
+  imports: [CommonModule, DashboardPageComponent, HomepageComponent],
   templateUrl: './landing-page.component.html',
 })
 export class LandingPageComponent {
